e7f9ee6d7b7c9ca1cb3a53860930b16a29fa2b77
Juan Cardenas <dccartagena@gmail.com>
2022-09-16 12:52:43 +0200

    ADD skeleton for rl_agent

--- rl_agent.py
+++ rl_agent.py
@@ -4,3 +4,65 @@ import torch.optim as optim
 import torch.functional as F
 
 import numpy as np
+
+class DQN:
+    def __init__(self, n_features, n_actions):
+        self.n_features = n_features
+        self.n_actions = n_actions
+
+        # Define structure for NN
+        pass
+
+    def forward(self):
+        pass
+
+class agent(DQN):
+
+    # Initialize RL agent
+    def __init__(self, n_features, n_actions, lr = 0.9999, gamma = 0.9999, param_reg = 0.5):
+
+        super().__init__(n_features, n_actions)
+
+        self.state = []
+        self.action = []
+        self.reward = []
+        self.risk = []
+
+        self.lr = lr
+        self.gamma = gamma
+        self.param_reg = param_reg # Regularization parameter
+
+        pass
+
+    # Replay memory
+    def replay_memory(self):
+        pass
+
+    # Compute risk from current state to fatal state
+    def get_risk(self):
+        # Compute shortest path
+        pass
+    
+    # Computer shortest path to fatal state
+    def get_shortest_path(self):
+        pass
+
+    # Compute reward + risk regularization
+    def get_reward(self):
+        # Get new reward
+
+        # Get risk from current state
+
+        # Compute regularized reward: reward + risk
+        pass
+
+    # Compute action
+    def get_action(self):
+        pass
+
+    # Train agent
+    def train(self, env):
+        pass
+    
+
+    
