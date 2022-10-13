af785d977a7085aa3e873a60a1fa2fc80cbdd109
Juan Cardenas <dccartagena@gmail.com>
2022-10-13 02:41:52 +0200

    replay memory class DONE

--- rl_agent.py
+++ rl_agent.py
@@ -4,6 +4,9 @@ import torch.optim as optim
 import torch.functional as F
 
 import numpy as np
+import random
+
+from collections import deque
 
 from shortest_path import tree
 
@@ -19,37 +22,48 @@ class DQN:
         pass
 
 class replay_memory:
+# Store the tuples (state, action, next state, reward) that the agent observes.
     def __init__(self, capacity):
+        # Initialize the memory
         self.capacity = capacity
-        # self.memory
+        self.memory = deque([], maxlen=self.capacity)
         pass
     
-    def store(self):
+    def store(self, state, action, next_state, reward, risk):
+        transition = (state, action, next_state, reward, risk)
+        self.memory.append(transition)
         pass
 
-    def sample(self):
-        pass
+    def sample(self, batch_size):
+        # Sample transition from memory
+        return random.sample(self.memory, batch_size)
+        
 
-    def __len__():
-        pass
-    
+    def __len__(self):
+        # length of memory
+        return len(self.memory)
 
-class agent():
+class agent(nn.Module):
 
     # Initialize RL agent
-    def __init__(self, n_features, n_actions, lr = 0.9999, gamma = 0.9999, param_reg = 0.5):
-
+    def __init__(self, n_features, n_actions, lr = 0.9999, gamma = 0.9999, beta = 0.5):
+        # Initialize NN module
+        super(agent, self).__init__
+        
+        # Store number of features and actions
         self.n_features = n_features
         self.n_actions = n_actions
 
+        # Initialize state, action, reward, and risk variables
         self.state = []
         self.action = []
         self.reward = []
         self.risk = []
 
-        self.lr = lr
-        self.gamma = gamma
-        self.param_reg = param_reg # Regularization parameter
+        # Store learning parametres
+        self.lr = lr        # Learning rate
+        self.gamma = gamma  # Discount factor
+        self.beta = beta    # Regularization parameter
 
         pass
 
