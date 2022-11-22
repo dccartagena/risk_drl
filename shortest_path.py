a949b6ca6d22aafcb148e9cc3b9837c1bf093f69
Juan Cardenas <dccartagena@gmail.com>
2022-11-22 14:51:37 +0100

    minor changes

--- shortest_path.py
+++ shortest_path.py
@@ -2,10 +2,11 @@ import numpy as np
 
 # TODO Define graph structure
 class graph:
-    def __init__(self):
-        self.node = []
-        self.cost = []
-        self.adj_matrix = []
+    def __init__(self, node, info, cost, adj_matrix):
+        self.node = node
+        self.info = info
+        self.cost = cost
+        self.adj_matrix = adj_matrix
         pass
     
     def len_vertex_set(self):
@@ -17,35 +18,65 @@ class graph:
         pass
     
     def modify_adj_matrix(self, new_edges):
-        # TODO modify adjencency matrix
         self.adj_matrix = []
         pass
     
-    def remove_node(self):
+    def remove_node(self, location):
+        
+        self.remove_edge(location)
         pass
     
-    def remove_edge(self):
+    def remove_edge(self, location):
         pass
     
     def modify_node(self, new_node):
         pass
+    
+    def get_len(self):
+        return len(self.node)
+    
+    def get_info(self, node):
+        return self.info(node)
 
 # TODO Use for Shortest path
 class tree:
     def __init__(self, graph, source):
+        self.graph = graph
         self.sourse = []
         self.ancestor = []
         self.descendant = []
+        
+        self.set_source(source)
         pass
+
+    def set_source(self, source):
+        '''
+        # Initialization
+        for each vertex v in graph.verices:
+            dist[v] <- infinity
+            prev[v] <- undefined
+            
+            # Create the vertex set
+            add v to Q
+        '''
+        self.sourse = source
+        
+        self.ancestor = np.nan * np.ones(self.graph.get_len())
+        self.dist = np.inf * np.ones(self.graph.get_len())
+        
+        self.set_nodes = self.graph.node
+        
+        '''
+        # Distance from source to source    
+        dist[source] <- 0
+        '''
+        self.dist[self.source] = 0
 
     def add_descendant(self, descendant):
         self.descendant.append(descendant)
 
     def set_ancestor(self, ancestor):
         self.ancestor = ancestor
-
-    def set_source(self, source):
-        self.source = source
 
     def get_source(self):
         return self.source
@@ -61,18 +92,9 @@ class tree:
         Pseudocode for Dijkstra algorithm
         input: graph, source_vertex
         output: vector_distance, vector_previous_nodes
+        '''      
         
-        # Initialization
-        for each vertex v in graph.verices:
-            dist[v] <- infinity
-            prev[v] <- undefined
-            
-            # Create the vertex set
-            add v to Q
-        
-        # Distance from source to source    
-        dist[source] <- 0
-        
+        '''
         # Compute shortest path
         while Q is non empty:
             # Get new vertex to compute
@@ -88,5 +110,10 @@ class tree:
                     
         return dist[], prev[]
         '''
+        while self.set_nodes:
+            node = self.nodes.pop()
+            
+            for 
+            pass 
         
         pass
